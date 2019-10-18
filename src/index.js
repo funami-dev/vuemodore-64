b4606f0a45ff13f6bc7c884457166497f04f97f3
Daniel Weber <dw@funami>
2019-10-19 00:53:10 +0200

    0.1.25

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import * as components from './components';
 // import C64Input from './components/Input/index.vue';
 // import C64Toggle from './components/Toggle/index.vue';
 
-export { components };
+export default { components };
