d4da69aff21307b81ce953c14a75c08b327334b1
Daniel Weber <dw@funami>
2019-10-18 22:40:09 +0200

    0.1.9

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import Vue from 'vue';
 import C64Input from './components/Input/index.vue';
 
-export default [C64Input];
+const Components = {
+  C64Input,
+};
+
+Object.keys(Components).forEach((name) => {
+  Vue.component(name, Components[name]);
+});
+
+export default Components;
