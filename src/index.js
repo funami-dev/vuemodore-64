d262199c15ea2e2b72a30287d6d837931369d5c0
Daniel Weber <dw@funami>
2019-10-19 00:43:44 +0200

    0.1.24

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-// import Vue from 'vue';
-import C64Input from './components/Input/index.vue';
-import C64Toggle from './components/Toggle/index.vue';
+import * as components from './components';
 
-// const Components = [C64Input, C64Toggle];
+// import C64Input from './components/Input/index.vue';
+// import C64Toggle from './components/Toggle/index.vue';
 
-// Object.keys(Components).forEach((name) => {
-//   Vue.component(name, Components[name]);
-// });
-
-export { C64Input, C64Toggle };
+export { components };
